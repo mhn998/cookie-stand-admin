e8524ff7c456d6012f3efba0646166c783828ac5
Muhannad <Muhannadalmughrabi233@gmail.com>
2021-06-29 19:15:08 +0300

    Done for changing json while typing

--- components/main.js
+++ components/main.js
@@ -1,64 +1,45 @@
-import { useState } from 'react'
-
 export default function CookieStandForm(props) {
 
-    const initialValues = {
-        location: '',
-        maxCustomers: 0,
-        minCustomers: 0,
-        avgCookies: 0,
-    };
+    const values = {
+        'location': props.values.location,
+        'maxCustomers': props.values.maxCustomers,
+        'minCustomers': props.values.minCustomers,
+        'avgCookies': props.values.avgCookies,
+    }; //Done
 
-    const jsonValues = JSON.stringify(initialValues)
+    const jsonValues = JSON.stringify(values)
 
-    const [values, setValues] = useState(initialValues);
-
-    function submitHandler(event) {
-        event.preventDefault();
-        setValues(initialValues)
-    }
-
-
-    function inputChangeHandler(event) {
-        let { name, value} = event.target;
-
-        setValues({values, [name]: value });
-    }
     return (
         <main>
-        <form className="grid items-center justify-center grid-cols-3 gap-5 p-10 mx-20 my-4 text-center bg-green-200 border-2 border-green-400 rounded-lg text-md gap-x-8" onSubmit={submitHandler}>
-            <div className="flex flex-col col-span-2 mb-4 ">
+        <form className="grid items-center justify-center grid-cols-4 gap-5 p-5 mx-20 my-4 text-center bg-green-200 border-2 border-green-400 rounded-lg text-md gap-x-8" onSubmit={props.submitHandler}>
+            <div className="flex flex-col col-span-8 mb-4 ">
                 <label className="mb-2 font-bold text-grey-darkest" htmlFor="location">Location</label>
-                <input className="px-3 py-2 mx-8 border text-grey-darkest" type="text" name="location" id="location" value={values.location} onChange={inputChangeHandler} placeholder="Cookie Stand Location" />
+                <input className="px-3 py-2 mx-8 border text-grey-darkest" type="text" name="location" id="location" onChange={props.changeHandler} placeholder="Cookie Stand Location" />
             </div>
 
-            <CookiesForm>
+            <div className="flex flex-col col-span-2">
                 <label className="mb-2 font-bold text-grey-darkest width" htmlFor="minCustomers">min Customers per Hour</label>
-                <input className="px-3 py-2 mx-8 border text-grey-darkest" type="number" name="minCustomers" id="minCustomers" value={values.minCustomers} onChange={inputChangeHandler} />
-            </CookiesForm>
-            <CookiesForm>
-                <label className="mb-2 font-bold text-grey-darkest" htmlFor="maxCustomers">max Customers per Hour</label>
-                <input className="px-3 py-2 mx-8 border text-grey-darkest" type="number" name="maxCustomers" id="maxCustomers" value={values.maxCustomers} onChange={inputChangeHandler} />
-            </CookiesForm>
-            <CookiesForm>
-                <label className="mb-2 font-bold text-grey-darkest" htmlFor="avgCookies">Average Cookies per Sale</label>
-                <input className="px-3 py-2 mx-8 border text-grey-darkest" type="number" name="avgCookies" id="avgCookies" value={values.avgCookies} onChange={inputChangeHandler} />
-            </CookiesForm>
+                <input className="px-3 mx-5 border text-grey-darkest" type="number" name="minCustomers" id="minCustomers" onChange={props.changeHandler} />
+            </div>
 
-            <button className="p-4 bg-green-500 rounded text-green hover:bg-green-600" type="submit" onSubmit={submitHandler}>Create</button>
+
+            <div className="flex flex-col col-span-2">
+                <label className="mb-2 font-bold text-grey-darkest" htmlFor="maxCustomers">max Customers per Hour</label>
+                <input className="px-3 mx-5 border text-grey-darkest" type="number" name="maxCustomers" id="maxCustomers"onChange={props.changeHandler} />
+            </div>
+        
+            <div className="flex flex-col col-span-2">
+                <label className="mb-2 font-bold text-grey-darkest" htmlFor="avgCookies">Average Cookies per Sale</label>
+                <input className="px-3 mx-5 border text-grey-darkest" type="number" name="avgCookies" id="avgCookies" onChange={props.changeHandler} />
+            </div>
+            
+
+            <button className="px-5 py-5 bg-green-500 rounded text-green hover:bg-green-600" type="submit" >Create</button>
         </form>
-        <div className="flex flex-col text-center mb-8">
+        <div className="flex flex-col mb-8 text-center">
             <placeholder className="mb-8">Report Table Coming Soon...</placeholder>
             <placeholder>{jsonValues}</placeholder>
         </div>
     </main>
-    );
-}
-
-function CookiesForm({ children }) {
-    return (
-        <div className="flex flex-col justify-between">
-            {children}
-        </div>
     );
 }
--- pages/index.js
+++ pages/index.js
@@ -7,11 +7,31 @@ import { useState } from 'react'
 
 export default function Home() {
 
+  const initialValues = {
+    location: '',
+    maxCustomers: 0,
+    minCustomers: 0,
+    avgCookies: 0,
+  };
+
+  const [values, setValues] = useState(initialValues);
+
+  function inputChangeHandler(event) {
+    const { name, value} = event.target;
+
+    setValues({...values, [name]: value });
+}
+
+  function createCookieHandler(event) {
+    event.preventDefault();
+    setValues(values)
+  }
+
   return (
     <div>
-      <Head/>
+      <Head title='Cookie Stand Admin'/>
       <Header title="Cookie Stand Admin" />
-      <Form  />
+      <Form values={values} submitHandler={createCookieHandler} changeHandler={inputChangeHandler}  />
       <Footer />
     </div>
   )
